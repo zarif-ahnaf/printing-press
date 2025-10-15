da25c5ad06764eb416fe8e2a9f06994fb10bee19
zarif-ahnaf <zombieahnaf@gmail.com>
2025-10-15 20:17:12 +0600

    add

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,20 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	esbuild: {
+		target: 'esnext',
+		legalComments: 'external'
+	},
+	build: {
+		commonjsOptions: {
+			transformMixedEsModules: true
+		},
+		chunkSizeWarningLimit: 2048,
+		emptyOutDir: true,
+		target: 'esnext',
+		cssTarget: 'esnext',
+		minify: 'terser'
+		//sourcemap: true
+	}
 });
