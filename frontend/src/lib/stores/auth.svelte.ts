2277cf61a63d89c8152c498eb9b9561fd0d4e64a
zarif-ahnaf <zombieahnaf@gmail.com>
2025-10-16 20:52:38 +0600

    add

--- frontend/src/lib/stores/auth.svelte.ts
+++ frontend/src/lib/stores/auth.svelte.ts
@@ -1,42 +1,20 @@
-import { USER_ENDPOINT } from '$lib/constants/backend';
-import { token } from './token.svelte';
-
 let isLoggedInState = $state<boolean | null>(null);
 let isAdminUser = $state<null | boolean>(null);
-
-$effect.root(() => {
-	if (token.value) {
-		fetch(USER_ENDPOINT, {
-			headers: {
-				Authorization: `Bearer ${token.value}`
-			}
-		})
-			.then(async (res) => {
-				if (res.ok) {
-					isLoggedInState = true;
-					const data = await res.json();
-					if (data && data.is_superuser) {
-						isAdminUser = true;
-					}
-				} else {
-					isLoggedInState = false;
-					token.set(null);
-				}
-			})
-			.catch(() => {
-				isLoggedInState = false;
-			});
-	}
-});
 
 export const is_logged_in = {
 	get value() {
 		return Boolean(isLoggedInState);
+	},
+	set(value: typeof isLoggedInState) {
+		isLoggedInState = value;
 	}
 };
 
 export const is_admin_user = {
 	get value() {
 		return Boolean(isAdminUser);
+	},
+	set(value: typeof isAdminUser) {
+		isAdminUser = value;
 	}
 };
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,8 +3,41 @@
 	import favicon from '$lib/assets/favicon.svg';
 	import { Toaster } from '$lib/components/ui/sonner/index.js';
 	import { ModeWatcher } from 'mode-watcher';
+	import { USER_ENDPOINT } from '$lib/constants/backend';
+	import { token } from '$lib/stores/token.svelte';
+	import { is_admin_user, is_logged_in } from '$lib/stores/auth.svelte';
 
 	let { children } = $props();
+
+	$effect.pre(() => {
+		const currentToken = token.value;
+
+		if (currentToken) {
+			fetch(USER_ENDPOINT, {
+				headers: {
+					Authorization: `Bearer ${currentToken}`
+				}
+			})
+				.then(async (res) => {
+					if (res.ok) {
+						is_logged_in.set(true);
+						const data = await res.json();
+						is_admin_user.set(data?.is_superuser);
+					} else {
+						is_logged_in.set(false);
+						is_admin_user.set(null);
+						token.set(null);
+					}
+				})
+				.catch(() => {
+					is_logged_in.set(false);
+					is_admin_user.set(null);
+				});
+		} else {
+			is_logged_in.set(false);
+			is_admin_user.set(null);
+		}
+	});
 </script>
 
 <svelte:head>
